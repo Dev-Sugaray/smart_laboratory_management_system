d7a4863a64d34eb24619a925f78d88d34bc94e10
Tega Sugaray <tegasugarayts@gmail.com>
2025-09-03 13:14:47 +0100

    fix: app nowworking again

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Serve all static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// For any other request, serve the main index.html file.
+// This is useful for single-page applications.
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
